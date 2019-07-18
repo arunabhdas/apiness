65fca803c6021728c93196ead1167fb273b45db7
Arunabh Das <arunabhdas@gmail.com>
2019-07-18 17:05:43 -0400

    Added Post entity

--- apiness-backend/apiness-spring/apiness/src/main/kotlin/com/arunabhdas/apiness/ApinessApplication.kt
+++ apiness-backend/apiness-spring/apiness/src/main/kotlin/com/arunabhdas/apiness/ApinessApplication.kt
@@ -9,3 +9,11 @@ class ApinessApplication
 fun main(args: Array<String>) {
 	runApplication<ApinessApplication>(*args)
 }
+
+@Entity
+data class Post {
+		@Id @GeneratedValue
+		val id: Int,
+		val title: String,
+		val content: String
+}
